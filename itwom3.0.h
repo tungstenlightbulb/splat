3f2587ddc911e54385e9b863a46cf8b7c0ad42dd
Peter Watkins <peter@watkinselectronics.com>
2016-04-08 09:33:22 -0700

    Pulling out main into separate file. removing dependencies on generated headers. Fixing some compiler warnings. Memory is now allocated on heap with 'new'

---
+++ itwom3.0.h
@@ -0,0 +1,14 @@
+//
+//  itwom3.0.h
+//  splat
+//
+//  Created by Peter Work Watkins on 6/17/15.
+//  Copyright (c) 2015 ke7ist. All rights reserved.
+//
+
+#ifndef splat_itwom3_0_h
+#define splat_itwom3_0_h
+
+double ITWOMVersion();
+
+#endif
